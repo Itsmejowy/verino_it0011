d8b6034aaaa75d92b18f9012e3641a86cdeb7f1e
Itsmejowy <202312482@fit.edu.ph>
2025-01-18 20:01:18 +0800

    hi

---
+++ [F1-TECHNICAL]/F1-Technical.py
@@ -0,0 +1,14 @@
+firstName = input("Enter First name: ")
+lastName = input("Enter Last name: ")
+middleName = input("Enter Middle name: ")
+birthday = input("Enter Birthday: ")
+gender = input("Enter Gender: ")
+address = input("Enter Address: ")
+course = input("Enter Course: ")
+yearLevel = input("Enter Year level: ")
+
+print("First name: " + firstName, "\nLast name: " + lastName, "\nMiddle name: " + middleName, "\nBirthday: " + birthday, "\nGender: " + gender, "\nAddress: " + address, "\nCourse: " + course, "\nYear level: " + yearLevel)
+
+
+
+
